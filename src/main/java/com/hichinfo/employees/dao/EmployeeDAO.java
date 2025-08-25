55449c5517340eaf4d0535f22247d1235e5d8fe6
Amine Hichri <amyn.hichri@gmail.com>
2025-08-25 15:35:03 +0200

    Add more methods in the DAO interface and implement them.

--- src/main/java/com/hichinfo/employees/dao/EmployeeDAO.java
+++ src/main/java/com/hichinfo/employees/dao/EmployeeDAO.java
@@ -7,4 +7,8 @@ import java.util.List;
 
 public interface EmployeeDAO {
     List<Employee> findAll();
+    Employee findById(long theId);
+    Employee save(Employee theEmployee);
+    void deleteById(long theId);
+
 }
