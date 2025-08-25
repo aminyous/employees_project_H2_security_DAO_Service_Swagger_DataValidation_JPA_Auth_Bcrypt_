8ef9e5453af89d70a8e501a8d51f04c6b6de4d5e
Amine Hichri <amyn.hichri@gmail.com>
2025-08-25 19:04:28 +0200

    Add post, put and delete controller

--- src/main/java/com/hichinfo/employees/controller/EmployeeRestController.java
+++ src/main/java/com/hichinfo/employees/controller/EmployeeRestController.java
@@ -1,18 +1,21 @@
 package com.hichinfo.employees.controller;
 
 import com.hichinfo.employees.entity.Employee;
+import com.hichinfo.employees.request.EmployeeRequest;
 import com.hichinfo.employees.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
+@Tag(name = "Employee Rest API Endpoints", description = "Operations related to employees.")
 public class EmployeeRestController {
 
 
@@ -23,9 +26,41 @@ public class EmployeeRestController {
     }
 
 
+    @Operation(summary = "Get all employees", description = "Retrieve a list of all employees.")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+
+    @Operation(summary = "Fetch single employee", description = "Get a single employee from database.")
+    @GetMapping("/{employeeId}")
+    @ResponseStatus(HttpStatus.OK)
+    public Employee getEmployee(@PathVariable @Min(value = 1) long employeeId){
+        return employeeService.findById(employeeId);
+    }
+
+    @Operation(summary = "Create a new employee", description = "Add a new employee to the DB.")
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee addEmployee(@Valid @RequestBody EmployeeRequest employeeRequest){
+        Employee employee = employeeService.save(employeeRequest);
+        return employee;
+    }
+
+    @Operation(summary = "Update an employee", description = "Update the details of a current employee.")
+    @PutMapping("/{employeeId}")
+    @ResponseStatus(HttpStatus.OK)
+    public Employee updateEmployee(@PathVariable @Min(value = 1) long employeeId,
+                                   @Valid  @RequestBody EmployeeRequest employeeRequest){
+        return employeeService.update(employeeId, employeeRequest);
+    }
+
+
+    @Operation(summary = "Delete an employee", description = "Remove an employee from the DB.")
+    @DeleteMapping("/{employeeId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteEmployee(@PathVariable @Min(value = 1) long employeeId){
+        employeeService.deleteById(employeeId);
     }
 }
