60e83108fe6c0894f4e7a74774f84605d651d1bb
Amine Hichri <amyn.hichri@gmail.com>
2025-09-01 13:58:36 +0200

    Add Spring Security - handle swagger basic authentication as it is no pratical to manager this in the browser (the default way)

--- src/main/java/com/hichinfo/employees/security/SecurityConfig.java
+++ src/main/java/com/hichinfo/employees/security/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.hichinfo.employees.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -45,11 +47,30 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
                         .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
                         .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
         );
+
+        http.httpBasic(httpBasicCustomizer -> httpBasicCustomizer.disable());
         http.httpBasic(Customizer.withDefaults());
         http.csrf(csrf -> csrf.disable());
+
+        http.exceptionHandling(exceptionHandling -> exceptionHandling
+                .authenticationEntryPoint(authenticationEntryPoint()));
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint(){
+        return (request, response, authException) -> {
+
+            // send 401 unauthorized status without triggering a basic auth
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+
+            // remove the WWW-Authenticate header to prevent browser popup
+            response.setHeader("WWW-Authenticate", "");
+            response.getWriter().write("{\"error\": \"Unauthorized access\"");
+        };
     }
 }
